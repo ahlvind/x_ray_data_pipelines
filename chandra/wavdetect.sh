c21a43693d949e87bc6133e2fcfe5b16b2fa01f1
Julia Ahlvind <ahlvind@kth.se>
2026-03-24 14:02:06 +0100

    Updating wavdetect with a psfmap the same size as small image focused on src region.

--- chandra/wavdetect.sh
+++ chandra/wavdetect.sh
@@ -6,8 +6,15 @@ obsid=$2
 cd "/Users/juliaahlvind/Documents/projekt_3/data/chandra/${obsid}/repro"
 
 # create psf map
-mkpsfmap infile="image_058_bin1.fits" outfile="mypsfmap_${sn_name}_058.fits" energy=1.49 ecf=0.90 clobber=yes
-
+echo "create psfmap if it does not exsitst"
+FILE="mypsfmap_058_${sn_name}_${obsid}.fits"
+if [ -e "$FILE" ]
+then
+    echo "Exposuremap already exists"
+else
+    echo "making exposuremap"
+    mkpsfmap infile="small_image_058_bin1_${sn_name}.fits" outfile="mypsfmap_058_${sn_name}_${obsid}.fits" energy=1.49 ecf=0.90 clobber=yes
+fi
 
 fits_file="image_058_bin1.fits"
 livetime_full=$(fitsheader "$fits_file" | grep "LIVETIME" | awk -F '=' '{print $2}' | tr -d ' ')
@@ -18,13 +25,13 @@ echo $livetime
 set clobber=yes
 wavdetect \
     infile="small_image_058_bin1_${sn_name}.fits" \
-    outfile="sources_058_wavdetect_${obsid}.fits" \
+    outfile="sources_058_wavdetect_${sn_name}.fits" \
     imagefile="image_058_wavdetect_${sn_name}.fits" \
     defnbkgfile="background058_${sn_name}.fits" \
-    scellfile="new_sources_058_${obsid}.fits" \
+    scellfile="new_sources_058_${sn_name}.fits" \
     scales="1.0 2.0 3.0" \
     exptime=$livetime \
-    psffile="mypsfmap_${sn_name}_058.fits" \
+    psffile="mypsfmap_058_${sn_name}_${obsid}.fits" \
     regfile="srcs_wavdetect_058_${sn_name}.reg" \
     sigthresh=1e-5 \
     clobber=yes
